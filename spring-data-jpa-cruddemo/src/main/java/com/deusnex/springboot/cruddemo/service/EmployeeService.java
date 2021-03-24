cfb777040d0a5a788af459e853a0e054e88f07d0
SaintEed <dsteed18@gmail.com>
2021-03-24 14:42:40 +0000

    Initial Commit

---
+++ spring-data-jpa-cruddemo/src/main/java/com/deusnex/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.deusnex.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.deusnex.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
---
+++ spring-data-jpa-cruddemo/src/main/java/com/deusnex/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.deusnex.springboot.cruddemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.deusnex.springboot.cruddemo.dao.EmployeeDAO;
+import com.deusnex.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired //@Qualifier used to determine which DAOImpl to use
+	public EmployeeServiceImpl (@Qualifier("employeeDAOJpaImpl")EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+	
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {
+		
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		employeeDAO.save(theEmployee);
+
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
+
+	}
+
+}
